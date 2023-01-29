62acc3cff999bfd5f996f49fac412601a7ea5d70
Valeria Martynova <kthf175@gmail.com>
2023-01-29 15:13:54 +0500

    add method fillArray

---
+++ firstBlock/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] arr = fillArray(4);
+Console.WriteLine(string.Join(", ", arr));
+
+string[] fillArray(int size)
+{   
+    string[] array = new string[size];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите {i+1} элемент массива");
+        array[i] = Console.ReadLine();
+    }
+
+    return array;
+}
